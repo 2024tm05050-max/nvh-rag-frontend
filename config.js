3eef37d3d322674fbaeac38835cbfcfea07d04ee
2024tm05050-max <2024tm05050@wilp.bits-pilani.ac.in>
2026-04-01 01:20:49 +0000

    add config with backend URL`

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // Change this URL to wherever your FastAPI backend is running
 const CONFIG = {
-    API_BASE_URL: "https://upgraded-zebra-x5qj94wrwjv9hj9v-8000.app.github.dev",
+    API_BASE_URL: "https://jubilant-winner-697jrpgvgxw4h67v-8000.app.github.dev",
     APP_NAME: "NVH Compliance Assistant"
 };
