31f8e69ba35754b9da1daf58b786a3a8beb125b7
2024tm05050-max <2024tm05050@wilp.bits-pilani.ac.in>
2026-04-01 13:48:49 +0000

    changed API based URL for frintend running

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // Change this URL to wherever your FastAPI backend is running
 const CONFIG = {
-    API_BASE_URL: "https://jubilant-winner-697jrpgvgxw4h67v-8000.app.github.dev",
+    API_BASE_URL: "https://legendary-space-winner-vppw46q6rv73w77q-8000.app.github.dev",
     APP_NAME: "NVH Compliance Assistant"
 };
